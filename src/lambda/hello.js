41b08c79b39ae09602e4c0cb3e1c171cf1fe5add
Kensaku Komtasu <kensaku.komatsu@gmail.com>
2018-08-13 22:43:15 -0700

    change lambda

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,7 +1,15 @@
-export function handler(event, context, callback) {
+// export function handler(event, context, callback) {
+//   console.log(event)
+//   callback(null, {
+//     statusCode: 200,
+//     body: JSON.stringify({msg: "Hello, World!"})
+//   })
+// }
+export async function handler(event) {
   console.log(event)
-  callback(null, {
+
+  return {
     statusCode: 200,
     body: JSON.stringify({msg: "Hello, World!"})
-  })
+  }
 }
